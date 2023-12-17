903c73fcc9c2ce6d4101f30e18fa5e0bbaa529fc
literallymyfault <literallymyfault@gmail.com>
2023-12-17 14:14:15 +0500

    fix

--- internal/modules/layer/layer.go
+++ internal/modules/layer/layer.go
@@ -1,1 +1,67 @@
 package layer
+
+import (
+	"fmt"
+	"github.com/rafailovalexey/service-generator/internal/modules/strategy"
+	"sync"
+)
+
+type LayerInterface interface {
+	IsExist(string) bool
+	GetLayer(string) (strategy.GenerationStrategyInterface, error)
+}
+
+type Layer struct {
+	dictionary map[string]strategy.GenerationStrategyInterface
+	rwmutex    sync.RWMutex
+}
+
+var _ LayerInterface = (*Layer)(nil)
+
+func NewLayer() *Layer {
+	dictionary := GetDictionary()
+
+	return &Layer{
+		dictionary: dictionary,
+		rwmutex:    sync.RWMutex{},
+	}
+}
+
+func (l *Layer) IsExist(value string) bool {
+	l.rwmutex.RLock()
+	defer l.rwmutex.RUnlock()
+
+	_, isExist := l.dictionary[value]
+
+	return isExist
+}
+
+func (l *Layer) GetLayer(value string) (strategy.GenerationStrategyInterface, error) {
+	l.rwmutex.RLock()
+	defer l.rwmutex.RUnlock()
+
+	element, isExist := l.dictionary[value]
+
+	if !isExist {
+		return nil, fmt.Errorf("element not found")
+	}
+
+	return element, nil
+}
+
+func GetDictionary() map[string]strategy.GenerationStrategyInterface {
+	dictionary := make(map[string]strategy.GenerationStrategyInterface)
+
+	dictionary["api"] = &strategy.RealisationGeneration{}
+	dictionary["controller"] = &strategy.RealisationGeneration{}
+	dictionary["service"] = &strategy.RealisationGeneration{}
+	dictionary["provider"] = &strategy.RealisationGeneration{}
+	dictionary["repository"] = &strategy.RealisationGeneration{}
+
+	dictionary["request"] = &strategy.DataTransferObjectGeneration{}
+	dictionary["response"] = &strategy.DataTransferObjectGeneration{}
+	dictionary["dto"] = &strategy.DataTransferObjectGeneration{}
+	dictionary["model"] = &strategy.DataTransferObjectGeneration{}
+
+	return dictionary
+}
--- internal/modules/strategy/strategy.go
+++ internal/modules/strategy/strategy.go
@@ -1,7 +1,44 @@
 package strategy
 
+import (
+	"github.com/rafailovalexey/service-generator/internal/modules/builder"
+	"github.com/rafailovalexey/service-generator/internal/modules/flags"
+)
 
+type GenerationStrategyInterface interface {
+	Generate(*flags.Flags) error
+}
 
-GetRealisation
+type DataTransferObjectGeneration struct{}
 
-GetDataTransferObject
+var _ GenerationStrategyInterface = (*DataTransferObjectGeneration)(nil)
+
+func (c *DataTransferObjectGeneration) Generate(f *flags.Flags) error {
+	err := builder.CreateDataTransferObject(f.Layer, f.Name)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type RealisationGeneration struct{}
+
+var _ GenerationStrategyInterface = (*RealisationGeneration)(nil)
+
+func (p *RealisationGeneration) Generate(f *flags.Flags) error {
+	err := builder.CreateLayer(f.Layer, f.Name)
+
+	if err != nil {
+		return err
+	}
+
+	err = builder.CreateImplementation(f.Layer, f.Name)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
